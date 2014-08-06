31036d7ddca4c3c63b72863151d19116b9069e0c
Shoichi Takagi <fly1tkg@gmail.com>
2014-08-06 13:57:18 +0900

    fix return_to uri

--- app/controllers/server_controller.rb
+++ app/controllers/server_controller.rb
@@ -11,7 +11,7 @@ class ServerController < ApplicationController
   <XRD>
     <Service priority="0">
       <Type>http://specs.openid.net/auth/2.0/return_to</Type>
-      <URI>https://73e4752b.ngrok.com</URI>
+      <URI>#{request.protocol}#{request.host_with_port}</URI>
     </Service>
   </XRD>
 </xrds:XRDS>
